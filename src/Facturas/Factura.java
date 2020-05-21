628f0279b3a2dc40f44bdae581fa35590f0d1796
Jorge <jorgefalquinajurado@gmail.com>
2020-05-21 08:38:33 +0200

    Descomentar enumerado

--- src/Facturas/Factura.java
+++ src/Facturas/Factura.java
@@ -5,7 +5,7 @@ public class Factura {
 	public int num;
 	public float base;
 	public float tipoIva;
-	//public Estados estado;
+	public Estados estado;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
